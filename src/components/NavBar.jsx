1cb1472ce8308d735357a08e3e41c67c42b0ccc0
tahenyBELGUITH <taheny.belguith@gmail.com>
2022-06-08 13:29:33 +0100

    Add the layout of components

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function NavBar() {
+  return (
+    <div>
+     <h1>TaHtouHa Store</h1>
+     <nav>
+       <ul>
+         <li>Books</li>
+         <li>Categories</li>
+       </ul>
+     </nav>
+    </div>
+  )
+}
