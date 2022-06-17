4dc608e7564bf90cb95ebe4933ab246ad7d493c7
tahenyBELGUITH <taheny.belguith@gmail.com>
2022-06-17 20:35:34 +0100

    Fetch API

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import './NavBar.css';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "./NavBar.css";
 
 export default function NavBar() {
   return (
-    <div className="panel-bg">
+    <div className="panel">
       <h1 className="Bookstore-CMS">TAHTOUHA</h1>
       <nav>
         <ul>
